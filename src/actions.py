10eda984b0ddbbf0f3023cecf82e9aacf9dd2a23
danhenrik <danihtoledo22@gmail.com>
2021-10-18 00:18:47 -0300

    Delete message first then send it

--- src/actions.py
+++ src/actions.py
@@ -2,19 +2,24 @@ from telegram.ext import DictPersistence
 from env import DEV_ID
 from datetime import datetime
 
-persistence = DictPersistence(store_bot_data=False, store_chat_data=True,
-                              store_user_data=False)
+persistence = DictPersistence(
+    store_bot_data=False, store_chat_data=True, store_user_data=False)
 
 
 def say(update, context, message, delete=0):
-    res = context.bot.send_message(
-        chat_id=update.effective_chat.id, text=message, parse_mode='HTML')
     if delete:
         delMessage = persistence.get_chat_data(
         )[update.effective_chat.id]
         if delMessage:
+            # Deletes the last message
             context.bot.delete_message(
                 update.effective_chat.id, delMessage['message_id'])
+    res = context.bot.send_message(
+        chat_id=update.effective_chat.id, text=message, parse_mode='HTML')
+    # Thosse two ifs are here just so the output comes out after the last message was deleted, if we didn't 
+    # separate in two the message would come in before the past goes out.
+    if delete:
+        # Place the current message as the next one
         persistence.update_chat_data(update.effective_chat.id, {
                                      "message_id": res.message_id})
 
